4f478eb14a35cde51bc26c77eee222a4b69e31dc
nmartineli <nataliamartineli@gmail.com>
2021-03-06 11:12:20 -0300

    Adicionada lista de Pokemon

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+
+const listaPokemons = [
+    'Bulbasaur',
+    'Ivysaur',	
+    'Venusaur',
+    'Mega Venusaur',
+    'Charmander',
+    'Charmeleon',
+    'Charizard',
+    'Mega Charizard X',
+    'Mega Charizard Y',
+    'Squirtle',
+    'Wartortle',
+    'Blastoise',
+    'Mega Blastoise',
+    'Caterpie',
+    'Metapod',
+    'Butterfree',
+    'Weedle',
+    'Kakuna',
+    'Beedrill',
+    'Mega Beedrill',
+];
+
+
